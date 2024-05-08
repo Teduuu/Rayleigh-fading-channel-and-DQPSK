9bb183924a0fce46cb261341dc33f51356a807eb
Teduuu <96039811+Teduuu@users.noreply.github.com>
2024-05-08 17:48:37 +0800

    Update fading.cpp

--- fading.cpp
+++ fading.cpp
@@ -11,19 +11,15 @@
 #define Wm 2*PI*80
 #define N 32.0
 #define N0 8.0
-
 using namespace std;
 
-
 int main() {
-    int j;
-    int NUM_BITS = 5000000;
-    int num_errors = 0;
-    double hi_t_real, hi_t_complex, hq_t_real, hq_t_complex;
-    double real_part, complex_part, rn,hi_t;
-
-	ofstream file("dbpsk_data.csv");
-	
+    	int j;
+    	int NUM_BITS = 5000000;
+   	int num_errors = 0;
+    	double hi_t_real, hi_t_complex, hq_t_real, hq_t_complex;
+    	double real_part, complex_part, rn,hi_t;
+	ofstream file("dbpsk_data.csv");	
 	for(j = 0; j<NUM_BITS; j++){
 		hi_t_real = h_t_real(j*Ts);
 		hq_t_complex = h_t_complex(j*Ts);
@@ -36,12 +32,11 @@ int main() {
     	file << j*Ts << "," << rn<< "," << phi <<","<< hi_t_real << "," << hq_t_complex <<endl;	
 	}
 	file.close();
-    return 0;
+    	return 0;
 }
 
 // Function to generate Gaussian-distributed random noise
 double gaussian_noise() {
-    
     double u1 = rand() / (RAND_MAX + 1.0);
     double u2 = rand() / (RAND_MAX + 1.0);
     return sqrt(-2.0 * log(u1)) * cos(2.0 * PI * u2);
@@ -56,7 +51,6 @@ double alpha(int i){
 double phi_nk(int i){
 	double phi_n = (i*PI)/N0;
 	return phi_n;
-	
 }
 
 // compute real part 
